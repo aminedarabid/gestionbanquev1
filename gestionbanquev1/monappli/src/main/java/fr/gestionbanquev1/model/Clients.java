79cb0766faac095de7837ebab27b442f0ac281fd
aminedarabid <amine.darabid@gmail.com>
2017-11-25 14:54:21 +0100

    cleanning

---
+++ gestionbanquev1/monappli/src/main/java/fr/gestionbanquev1/model/Clients.java
@@ -0,0 +1,42 @@
+package fr.gestionbanquev1.model;
+
+/**
+ * @author Amine DARABID
+ *
+ */
+public class Clients {
+
+	private int id;
+	private String nom;
+	private String prenom;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom.toUpperCase();
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public Clients(int id, String nom, String prenom) {
+		this.id = id;
+		this.nom = nom.toUpperCase();
+		this.prenom = prenom;
+	}
+}
